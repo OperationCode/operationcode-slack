a38545232ac67ac089d4f1291af711949e90b789
RickR <rrein@akamai.com>
2017-01-04 10:55:14 -0700

    populate user data if given a string

--- lib/operationcode/slack/user.rb
+++ lib/operationcode/slack/user.rb
@@ -7,10 +7,16 @@ module Operationcode
         # If we get a string should we go look the users info up?
         if data.is_a? String
           @id = data
+          populate_name!
         else
           @id = data['id']
           @name = data['name']
         end
+      end
+
+      def populate_name!
+        res = Operationcode::Slack::Api::UsersInfo.post(with_data: { user: @id })
+        @name = res['user']['real_name'] if res['user']
       end
     end
   end
--- test/operationcode/slack/user_test.rb
+++ test/operationcode/slack/user_test.rb
@@ -2,17 +2,21 @@ require 'test_helper'
 
 class Operationcode::Slack::UserTest < Minitest::Test
   def test_can_be_instantiated
-    user = Operationcode::Slack::User.new('some data')
+    user = Operationcode::Slack::User.new({id: 'fakeid', name: 'fakename' })
     assert_instance_of Operationcode::Slack::User, user
   end
 
   def test_can_be_passed_a_hash_or_string
+    Operationcode::Slack::Api::UsersInfo.expects(:post)
+      .with(with_data: { user: 'JUST_A_USER_ID' }).once
+      .returns({ 'ok' => 'true', 'user' => { 'real_name' => 'RETURNED_NAME' } })
+
     user = Operationcode::Slack::User.new({ 'id' => 'FAKE_ID', 'name' => 'FAKE_NAME' })
     assert_equal 'FAKE_ID', user.id
     assert_equal 'FAKE_NAME', user.name
 
     user = Operationcode::Slack::User.new('JUST_A_USER_ID')
     assert_equal 'JUST_A_USER_ID', user.id
-    assert_nil user.name
+    assert_equal 'RETURNED_NAME', user.name
   end
 end
